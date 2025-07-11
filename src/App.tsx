b2c9982e4802487d1238460cd547336b9f510893
Manuela <bognarmanuela23@gmail.com>
2025-07-11 17:07:02 -0300

    component Badge

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Text from "./components/text";
 import TrashIcon from "./assets/icons/trash.svg?react";
 import Icon from "./components/icon";
 import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import Badge from "./components/badge";
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <Text variant="body-sm-bold" className="text-pink-base">hello world!</Text>
       <Icon svg={TrashIcon} className="fill-green-base" />
       <Icon svg={SpinnerIcon} animate />
+
+      <Badge variant= "secondary">5</Badge>
+      <Badge variant= "primary">2 de 5</Badge>
     </>
   )
 }
