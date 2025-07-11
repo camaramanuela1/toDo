09d558d2a38b3e71e49a24b724d05bbc2f08a533
Manuela <bognarmanuela23@gmail.com>
2025-07-11 16:17:23 -0300

    component icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Text from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import Icon from "./components/icon";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
 
 function App() {
   return (
     <>
       <Text variant="body-sm-bold" className="text-pink-base">hello world!</Text>
+      <Icon svg={TrashIcon} className="fill-green-base" />
+      <Icon svg={SpinnerIcon} animate />
     </>
   )
 }
