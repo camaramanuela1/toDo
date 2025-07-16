66bb17745b22e724355fd934df972d39beb66d98
Manuela <bognarmanuela23@gmail.com>
2025-07-16 20:01:19 -0300

    component Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
 import InputText from "./components/input";
 import InputCheckbox from "./components/checkbox";
+import Skeleton from "./components/skeleton";
 
 import TrashIcon from "./assets/icons/trash.svg?react";
 import PlusIcon from "./assets/icons/plus.svg?react";
@@ -14,28 +15,41 @@ import Container from "./components/container";
 
 function App() {
   return (
-    <Container>
+    <Container className="space-y-6">
       <Text variant="body-sm-bold" className="text-pink-base">hello world!</Text>
       <Icon svg={TrashIcon} className="fill-green-base" />
       <Icon svg={SpinnerIcon} animate />
 
-      <Badge variant= "secondary">5</Badge>
-      <Badge variant= "primary">2 de 5</Badge>
+      <div className="flex gap-1">
+          <Badge variant= "secondary">5</Badge>
+          <Badge variant= "primary">2 de 5</Badge>
+          <Badge loading>5</Badge>
+      </div>
+      
 
       <Button icon={PlusIcon}>Nova tarefa</Button>
 
-      <ButtonIcon icon={TrashIcon} />
-      <ButtonIcon icon={TrashIcon} variant="secondary" />
-      <ButtonIcon icon={TrashIcon} variant="terciary" />
+      <div className="flex gap-1">
+        <ButtonIcon icon={TrashIcon} />
+        <ButtonIcon icon={TrashIcon} variant="secondary" />
+        <ButtonIcon icon={TrashIcon} variant="terciary" />
+        <ButtonIcon icon={TrashIcon} loading/>
+      </div>
 
-      <br/><br/>
+      <br />
       <InputText />
 
-      <br /><br />
+      <br />
       <InputCheckbox />
+      <InputCheckbox loading />
 
-      <br /><br /> 
       <Card size="md">Hello Card!</Card>
+
+      <div className="space-y-2">
+        <Skeleton className="h-6"/>
+        <Skeleton className="h-6"/>
+        <Skeleton className="w-96 h-6"/>
+      </div>
     </Container>
   )
 }
--- src/components/button-icon.tsx
+++ src/components/button-icon.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Icon from "./icon";
 import {cva, type VariantProps} from "class-variance-authority"
+import Skeleton from "./skeleton";
 
 export const buttonIconVariants = cva(`
     inline-flex items-center justify-center cursor-pointer transition group
 `, {
     variants: {
         variant: {
+            none: "",
             primary: "bg-green-base hover:bg-green-dark",
             secondary: "bg-gray-200 hover:bg-pink-base",
             terciary: "bg-transparent hover:bg-gray-200"
@@ -28,6 +30,7 @@ export const buttonIconVariants = cva(`
 export const buttonIconIconVariant = cva("transition", {
     variants: {
         variant: {
+            none: "",
             primary: "fill-white",
             secondary: "fill-pink-base group-hover:fill-white",
             terciary: "fill-gray-300 group-hover:fill-gray-400"
@@ -46,7 +49,8 @@ interface ButtonIconProps
 extends VariantProps<typeof buttonIconVariants>, 
 Omit<React.ComponentProps<"button">, "size" | "disabled"> 
 {
-    icon: React.ComponentProps<typeof Icon>["svg"]
+    icon: React.ComponentProps<typeof Icon>["svg"];
+    loading?: boolean;
 }
 
 export default function ButtonIcon({
@@ -55,8 +59,13 @@ export default function ButtonIcon({
     disabled,
     className,
     icon,
+    loading,
     ...props
 }: ButtonIconProps){
+    if (loading) {
+        return <Skeleton rounded="sm" className={buttonIconVariants({variant: "none", size, className})} />
+    }
+
     return (
         <button className={buttonIconVariants({
             variant,
