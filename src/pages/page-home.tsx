511dfbf3aaeee856ceb317a2a75ca215dc5a298d
Manuela <bognarmanuela23@gmail.com>
2025-07-17 16:31:41 -0300

    creating task summary component

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,5 +1,10 @@
 import Container from "../components/container";
+import TasksSummary from "../core-components/taks-summary";
 
 export default function PageHome(){
-    return <Container>Home page!</Container>
+    return <Container as="article" className="space-y-3">
+        <header className="flex items-center justify-between">
+            <TasksSummary />
+        </header>
+    </Container>
 }
